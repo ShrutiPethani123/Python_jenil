b67a2f5a794035ca728fce1dbab09957e7d96cb5
unknown <shrutipethani.royal@gmail.com>
2023-04-10 16:49:16 +0530

    operator

---
+++ ifElse.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a no: "))
+
+if a>0:
+    print("Positive")
+elif (a==0):
+    print("Zero")
+else:
+    # pass
+    print("Negative")
+
+print('Thank You!!')
---
+++ operators.py
@@ -0,0 +1,36 @@
+'''
+Operators:
+
+a+b
+
+a, b: operand
++ : operator
+
+1. Arithmetic Operator: +  - * / % // **
+2. Logical Operator: and or not
+3. Relational Operator: < > <= >= == !=
+4. Assignment Operator:  = += -= *= /= %= ....
+5. Bitwise Operator: & | ^ ~ << >>
+6. Membership Operator: in , not in
+7. Identity Operator: is , is not
+
+'''
+
+# 1. Arithmetic Operator: +  - * / % // **
+
+a=int(input('Enter a no:'))
+b=int(input('Enter a no:'))
+
+
+print("sum: ",a+b)
+print("mul: ",a*b)
+print("sub: ",a-b)
+print("div: ",a/b)
+print("mod: ",a%b)
+print("floor division: ",a//b)
+print("Exponant: ",a**b)
+
+print(3**4)
+
+
+
